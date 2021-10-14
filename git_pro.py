f97ac510283a748a1b3ae012353c9358bb17d0a4
hanezki <hanezki.manninen@gmail.com>
2021-10-14 15:54:04 +0300

    lisättiin person luokka

--- git_pro.py
+++ git_pro.py
@@ -1,3 +1,37 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_age(self, age):
+        self.age = age
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
+
+def edit_person(person1, name, age):
+    person.set_age(age)
+    person.set_name(name)
+
+
 def hello():
-    return "Hello dev!, mergetetty"
+    return "Hello dev!"
+
+
+if __name__ == "__main__":
+    person = Person("hannu", 24)
+    print(f"{person.name}, {person.age}")
+    edit_person(person, "jyrki", "100")
+    print(f"{person.name}, {person.age}")
+
+
+
+
 
